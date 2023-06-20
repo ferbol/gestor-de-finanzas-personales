0de93462e05d7cc2647eb3d041bea43fc8461f6d
Fernando <ferbol2014@hotmail.com>
2023-06-20 12:11:38 -0500

    Ultimo comit en PC

---
+++ supabase/client.js
@@ -0,0 +1,4 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://ggrrvvuihiubvqptsnnd.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdncnJ2dnVpaGl1YnZxcHRzbm5kIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NjY3MTI3NywiZXhwIjoyMDAyMjQ3Mjc3fQ.UUmDpDyJEW94jGzD_d9XxgqTtUXHN_brf9NEPNoL9qo");
+
